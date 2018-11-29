c425320065d70a06c407b3c84fffb7c29c501d47
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-30 00:16:51 +0530

    Add the build version which will be served

---
+++ public/build/precache-manifest.57c119d219758af7b2c563d9969d4029.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de10c8882f90c4063635",
+    "url": "/static/js/main.de10c888.chunk.js"
+  },
+  {
+    "revision": "4f26a9d47d20ba3d3377",
+    "url": "/static/js/1.4f26a9d4.chunk.js"
+  },
+  {
+    "revision": "de10c8882f90c4063635",
+    "url": "/static/css/main.0d0ddb22.chunk.css"
+  },
+  {
+    "revision": "856a561c7d0e990897484d0a50cc57ad",
+    "url": "/index.html"
+  }
+];
